515d3f37c762348f11f6de597250126801eab2bf
Kristifor Milchev <km@airzen.io>
2024-11-21 20:21:54 +0200

    Implementing the business rules to load the active rpc either from override or env file.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 
 	"oracle/commands"
@@ -10,6 +12,11 @@ import (
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	var rootCmd = &cobra.Command{Use: "oracle"}
 	walletService := &services.WalletService{
 		PasswordManager: &services.PasswordManager{},
@@ -23,7 +30,9 @@ func main() {
 	}
 	registerCommand := commands.RegisterCommand{
 		WalletService: walletService,
+
 		RegisterService: &services.RegisterService{
+			ContractAddr:  os.Getenv("CONTRACT_ADDRESS"),
 			WalletService: walletService,
 			RpcService:    rpcService,
 			VerificationService: &services.IdentityService{
@@ -37,8 +46,23 @@ func main() {
 	rootCmd.AddCommand(registerCommand.Executable())
 	rootCmd.AddCommand(syncCommand.Executable())
 
+	rpc := getRpc()
+	rpcService.SetClient(rpc)
+
 	if err := rootCmd.Execute(); err != nil {
 		// fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func getRpc() *string {
+	rpc := os.Getenv("RPC")
+
+	overriden, err := os.ReadFile("oracle-rpc")
+	if err != nil {
+		return &rpc
+	}
+
+	converted := string(overriden)
+	return &converted
 }
