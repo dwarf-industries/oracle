750916648ad8999739f4f17f082d94a976ab2732
Kristifor Milchev <km@airzen.io>
2024-11-25 13:21:32 +0200

    Adding better description when importing existing wallets.

--- commands/add_wallet.go
+++ commands/add_wallet.go
@@ -1,8 +1,13 @@
 package commands
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/fatih/color"
+	"github.com/jedib0t/go-pretty/table"
 	"github.com/spf13/cobra"
 
 	"oracle/interfaces"
@@ -30,5 +35,39 @@ func (aw *AddWalletCommand) Executable() *cobra.Command {
 }
 
 func (aw *AddWalletCommand) Execute(wallet *string, password *string) {
-	aw.WalletService.SetWallet(wallet, password)
+	privateKey, err := aw.WalletService.SetWallet(wallet, password)
+	if err != nil {
+		fmt.Println("Failed to import wallet!")
+		fmt.Println(err)
+		os.Exit(1)
+		return
+	}
+	fmt.Println("")
+	publicKeyHex := aw.WalletService.GetAddressForPrivateKey(&privateKey)
+	privateKeyHex := privateKey.D.Text(16)
+
+	header := color.New(color.FgCyan, color.Bold).SprintFunc()
+	fmt.Println(header("🚀 Wallet Imported Successful!"))
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Key Type", "Key Value"})
+	t.AppendRow(table.Row{"Public Key", publicKeyHex})
+	t.AppendRow(table.Row{"Private Key", privateKeyHex})
+	t.Render()
+
+	warning := color.New(color.FgRed, color.Bold).SprintFunc()
+	fmt.Println(warning("\n⚠️  IMPORTANT: Keep a secure copy of your private key. It is required for wallet recovery and cannot be retrieved if lost."))
+
+	walletBalance := aw.WalletService.GetBalance((*common.Address)(common.FromHex(publicKeyHex)))
+
+	if walletBalance.Int64() == 0 {
+		fmt.Println("\n⚠️  ATTENTION: it appears that you wallet balance is empty, you will need to fund it in order to use it as operations wallet!")
+	}
+
+	fmt.Println("you can learn more about funding and why it's required under 'oracle network-operation-info'")
+	infoHeader := color.New(color.BgCyan, color.Bold).SprintFunc()
+	fmt.Println(infoHeader("for more information regarding wallets 'oracle wallets-info'"))
+
+	os.Exit(0)
 }
