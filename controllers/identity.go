6bd77babf411ec44075fbe40369a25edb07c4a4d
Kristifor Milchev <km@airzen.io>
2024-11-25 16:39:52 +0200

    Adding a method to issue certificate challanges.

--- controllers/identity.go
+++ controllers/identity.go
@@ -4,9 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"oracle/interfaces"
 )
 
 type IdentityController struct {
+	VerificationService interfaces.VerificationService
+	IdentityService     interfaces.IdentityVerificationService
 }
 
 func (i *IdentityController) self(ctx *gin.Context) {
@@ -17,8 +21,25 @@ func (i *IdentityController) self(ctx *gin.Context) {
 
 }
 
+func (i *IdentityController) challange(ctx *gin.Context) {
+	var certificate []byte
+	if err := ctx.Bind(&certificate); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Denied": "Bad Request"})
+		return
+	}
+
+	challange, err := i.VerificationService.GenerateChallenge(certificate)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Message": "Bad Request"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, challange)
+}
+
 func (i *IdentityController) Init(r *gin.RouterGroup) {
 	controller := r.Group("identity")
 
 	controller.POST("/self", i.self)
+	controller.POST("/certificate", i.challange)
 }
