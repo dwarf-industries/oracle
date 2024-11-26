71f64126919bfa881cd76a5daa9ddeee51f6e0e4
Kristifor Milchev <km@airzen.io>
2024-11-26 12:15:17 +0200

    Adding a missing method to the verification interface.

--- interfaces/verification_service.go
+++ interfaces/verification_service.go
@@ -3,4 +3,5 @@ package interfaces
 type VerificationService interface {
 	GenerateChallenge(certificate []byte) ([]byte, error)
 	VerifyChallange(certificate []byte, signedChallenge []byte) (*string, error)
+	Init()
 }
