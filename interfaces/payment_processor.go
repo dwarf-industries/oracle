be1650ed0b2020a13bcd959a15369d8ae6ee87c8
Kristifor Milchev <km@airzen.io>
2024-11-26 17:53:32 +0200

    Adding a payment processor.

---
+++ interfaces/payment_processor.go
@@ -0,0 +1,10 @@
+package interfaces
+
+import (
+	"oracle/models"
+)
+
+type PaymentProcessor interface {
+	GeneratePaymentRequest(dataSize int) models.PaymentRequest
+	VerifyPayment(expectedPaymentID string) (bool, error)
+}
---
+++ services/payment_processor.go
@@ -0,0 +1,65 @@
+package services
+
+import (
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/crypto"
+
+	"oracle/interfaces"
+	"oracle/models"
+)
+
+type PaymentProcessor struct {
+	WalletService    interfaces.WalletService
+	expectedPayments map[string]bool
+}
+
+func (p *PaymentProcessor) GeneratePaymentRequest(dataSize int) models.PaymentRequest {
+	amount := big.NewInt(int64(dataSize))
+	wallet, err := p.WalletService.ActiveWallet()
+	if err != nil {
+		panic("wallet doesn't exist")
+	}
+	_, nodePaymentIdentifier, err := p.generatePaymentID(wallet)
+	if err != nil {
+		panic("failed to generate node payment identifier")
+	}
+	hash := sha256.Sum256([]byte(fmt.Sprintf("%d-%s", dataSize, nodePaymentIdentifier)))
+
+	paymentID := hex.EncodeToString(hash[:])
+	p.expectedPayments[paymentID] = false
+	return models.PaymentRequest{
+		PaymentID: paymentID,
+		Amount:    amount,
+	}
+}
+
+func (p *PaymentProcessor) VerifyPayment(expectedPaymentID string) (bool, error) {
+	isPaid := p.expectedPayments[expectedPaymentID]
+	if isPaid {
+		delete(p.expectedPayments, expectedPaymentID)
+		return false, nil
+	}
+
+	delete(p.expectedPayments, expectedPaymentID)
+	return true, fmt.Errorf("paymentID mismatch")
+}
+
+func (p *PaymentProcessor) generatePaymentID(nodePrivateKey *ecdsa.PrivateKey) (string, string, error) {
+	timestamp := time.Now().UnixNano()
+	rawID := fmt.Sprintf("PAY-%d", timestamp)
+
+	hash := sha256.Sum256([]byte(rawID))
+
+	signature, err := crypto.Sign(hash[:], nodePrivateKey)
+	if err != nil {
+		return "", "", err
+	}
+
+	return rawID, hex.EncodeToString(signature), nil
+}
