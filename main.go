f8fe25b9b4457f179e0c64f4e3e097a9e8ee6a22
Kristifor Milchev <km@airzen.io>
2024-12-08 17:20:14 +0200

    Adding graceful shutdown and a logo header to render with the start of the node.

--- main.go
+++ main.go
@@ -4,13 +4,54 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/glamour"
 	"github.com/spf13/cobra"
 
 	"oracle/commands"
 	"oracle/di"
 )
 
+func logo() {
+	in := `
+     ████████▄     ▄████████    ▄████████    ▄████████ ███▄▄▄▄   ████████▄
+     ███   ▀███   ███    ███   ███    ███   ███    ███ ███▀▀▀██▄ ███   ▀███
+     ███    ███   ███    █▀    ███    █▀    ███    █▀  ███   ███ ███    ███
+     ███    ███  ▄███▄▄▄      ▄███▄▄▄      ▄███▄▄▄     ███   ███ ███    ███
+     ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀     ▀▀███▀▀▀     ███   ███ ███    ███
+     ███    ███   ███    █▄    ███          ███    █▄  ███   ███ ███    ███
+     ███   ▄███   ███    ███   ███          ███    ███ ███   ███ ███   ▄███
+     ████████▀    ██████████   ███          ██████████  ▀█   █▀  ████████▀
+
+     ████████▄     ▄████████ ███▄▄▄▄   ▄██   ▄
+     ███   ▀███   ███    ███ ███▀▀▀██▄ ███   ██▄
+     ███    ███   ███    █▀  ███   ███ ███▄▄▄███
+     ███    ███  ▄███▄▄▄     ███   ███ ▀▀▀▀▀▀███
+     ███    ███ ▀▀███▀▀▀     ███   ███ ▄██   ███
+     ███    ███   ███    █▄  ███   ███ ███   ███
+     ███   ▄███   ███    ███ ███   ███ ███   ███
+     ████████▀    ██████████  ▀█   █▀   ▀█████▀
+
+     ████████▄     ▄████████    ▄███████▄  ▄██████▄     ▄████████    ▄████████
+     ███   ▀███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███
+     ███    ███   ███    █▀    ███    ███ ███    ███   ███    █▀    ███    █▀
+     ███    ███  ▄███▄▄▄       ███    ███ ███    ███   ███         ▄███▄▄▄
+     ███    ███ ▀▀███▀▀▀     ▀█████████▀  ███    ███ ▀███████████ ▀▀███▀▀▀
+     ███    ███   ███    █▄    ███        ███    ███          ███   ███    █▄
+     ███   ▄███   ███    ███   ███        ███    ███    ▄█    ███   ███    ███
+     ████████▀    ██████████  ▄████▀       ▀██████▀   ▄████████▀    ██████████
+
+	`
+
+	r, _ := glamour.NewTermRenderer(
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(120))
+
+	out, _ := r.Render(in)
+	fmt.Print(out)
+}
+
 func main() {
+	logo()
 	di.SetupServices()
 	var rootCmd = &cobra.Command{
 		Use: "oracle",
@@ -47,6 +88,28 @@ func main() {
 	rootCmd.AddCommand(generateWalletCommand.Executable())
 
 	if err := rootCmd.Execute(); err != nil {
+		oracle, err := di.RegisterService().GetOracle()
+		if err != nil {
+			fmt.Println("failed to shutdown gracefully.")
+			fmt.Println("Can't retrive oracle state on chain.")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if !oracle.ConnectionStatus {
+			fmt.Println("Shutting down, node state is already offline on chain")
+			os.Exit(0)
+		}
+
+		err = di.RegisterService().LogOut()
+		if err != nil {
+			fmt.Println("failed to shutdown gracefully.")
+			fmt.Println("Can't update oracle state on chain.")
+			fmt.Println("This is a dangerious situtation, oracles that remain online after being shutdown will get peanalized reach out to the development team!")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
 		fmt.Println(err)
 		os.Exit(1)
 	}
