cc7f2474adf621676e82bd30d773d00e505bdb15
Kristifor Milchev <km@airzen.io>
2024-12-09 16:23:18 +0200

    Refactoring the logo rendering.

--- main.go
+++ main.go
@@ -4,57 +4,27 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/glamour"
 	"github.com/spf13/cobra"
 
 	"oracle/commands"
+	"oracle/components"
 	"oracle/di"
 )
 
-func logo() {
-	in := `
-     ████████▄     ▄████████    ▄████████    ▄████████ ███▄▄▄▄   ████████▄
-     ███   ▀███   ███    ███   ███    ███   ███    ███ ███▀▀▀██▄ ███   ▀███
-     ███    ███   ███    █▀    ███    █▀    ███    █▀  ███   ███ ███    ███
-     ███    ███  ▄███▄▄▄      ▄███▄▄▄      ▄███▄▄▄     ███   ███ ███    ███
-     ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀     ▀▀███▀▀▀     ███   ███ ███    ███
-     ███    ███   ███    █▄    ███          ███    █▄  ███   ███ ███    ███
-     ███   ▄███   ███    ███   ███          ███    ███ ███   ███ ███   ▄███
-     ████████▀    ██████████   ███          ██████████  ▀█   █▀  ████████▀
-
-     ████████▄     ▄████████ ███▄▄▄▄   ▄██   ▄
-     ███   ▀███   ███    ███ ███▀▀▀██▄ ███   ██▄
-     ███    ███   ███    █▀  ███   ███ ███▄▄▄███
-     ███    ███  ▄███▄▄▄     ███   ███ ▀▀▀▀▀▀███
-     ███    ███ ▀▀███▀▀▀     ███   ███ ▄██   ███
-     ███    ███   ███    █▄  ███   ███ ███   ███
-     ███   ▄███   ███    ███ ███   ███ ███   ███
-     ████████▀    ██████████  ▀█   █▀   ▀█████▀
-
-     ████████▄     ▄████████    ▄███████▄  ▄██████▄     ▄████████    ▄████████
-     ███   ▀███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███
-     ███    ███   ███    █▀    ███    ███ ███    ███   ███    █▀    ███    █▀
-     ███    ███  ▄███▄▄▄       ███    ███ ███    ███   ███         ▄███▄▄▄
-     ███    ███ ▀▀███▀▀▀     ▀█████████▀  ███    ███ ▀███████████ ▀▀███▀▀▀
-     ███    ███   ███    █▄    ███        ███    ███          ███   ███    █▄
-     ███   ▄███   ███    ███   ███        ███    ███    ▄█    ███   ███    ███
-     ████████▀    ██████████  ▄████▀       ▀██████▀   ▄████████▀    ██████████
-
-	`
-
-	r, _ := glamour.NewTermRenderer(
-		glamour.WithAutoStyle(),
-		glamour.WithWordWrap(120))
-
-	out, _ := r.Render(in)
-	fmt.Print(out)
-}
-
 func main() {
-	logo()
 	di.SetupServices()
+
 	var rootCmd = &cobra.Command{
 		Use: "oracle",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				logo := components.Logo{}
+				logo.Init()
+
+				_ = cmd.Help()
+				os.Exit(0)
+			}
+		},
 	}
 
 	addWalletcommand := commands.AddWalletCommand{
@@ -81,6 +51,9 @@ func main() {
 		PasswordManager:     di.GetPasswordManager(),
 	}
 
+	port := ":4445"
+	syncCommand.Execute(&port)
+
 	rootCmd.AddCommand(addWalletcommand.Executable())
 	rootCmd.AddCommand(rpcCommand.Executable())
 	rootCmd.AddCommand(registerCommand.Executable())
@@ -88,27 +61,6 @@ func main() {
 	rootCmd.AddCommand(generateWalletCommand.Executable())
 
 	if err := rootCmd.Execute(); err != nil {
-		oracle, err := di.RegisterService().GetOracle()
-		if err != nil {
-			fmt.Println("failed to shutdown gracefully.")
-			fmt.Println("Can't retrive oracle state on chain.")
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		if !oracle.ConnectionStatus {
-			fmt.Println("Shutting down, node state is already offline on chain")
-			os.Exit(0)
-		}
-
-		err = di.RegisterService().LogOut()
-		if err != nil {
-			fmt.Println("failed to shutdown gracefully.")
-			fmt.Println("Can't update oracle state on chain.")
-			fmt.Println("This is a dangerious situtation, oracles that remain online after being shutdown will get peanalized reach out to the development team!")
-			fmt.Println(err)
-			os.Exit(1)
-		}
 
 		fmt.Println(err)
 		os.Exit(1)
